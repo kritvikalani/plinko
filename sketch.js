519233dc4ac2eed149971185a2675da9a6f43a26
kritvikalani <67899380+kritvikalani@users.noreply.github.com>
2020-08-12 20:11:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -43,6 +43,16 @@ for (var j = 40; j <= width; j= j+50)
    plinkos.push(new Plinko(j, 175))
  }
 
+ for (var j = 40; j <= width-10; j= j+50)
+ {
+   plinkos.push(new Plinko(j, 275))
+ }
+
+ for (var j = 15; j <= width-10; j= j+50)
+ {
+   plinkos.push(new Plinko(j, 375))
+ }
+
  if (frameCount % 60 === 0) {
   particles.push(new Particle(random(width/2-10, width/2+10),10,10));
  }
