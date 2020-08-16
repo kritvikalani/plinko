3e078b9219be5d168ea738e6b9c244b16defefda
kritvikalani <67899380+kritvikalani@users.noreply.github.com>
2020-08-16 22:27:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,22 +15,15 @@ function setup() {
   engine= Engine.create();
   world= engine.world;
 
-  ground= new Ground(240, 785, 480, 20)
+  ground= new Ground(240, 795, 480, 20)
 
  // plinko= new Plinko(200, 200, 5, 5)
 
   //particle= new Particle(200,200,20)
 
   //division= new Division(240,400,20,480)
-
-  Engine.run(engine);
-}
-
-function draw() {
-  background(0);  
-
   for (var k= 0; k<=width; k= k+80) {
-    divisions.push(new Division(k, height*divisionHeight/2, 10, divisionHeight));
+    divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
   }
 
 for (var j = 40; j <= width; j= j+50)
@@ -52,24 +45,29 @@ for (var j = 40; j <= width; j= j+50)
  {
    plinkos.push(new Plinko(j, 375))
  }
+}
 
- if (frameCount % 60 === 0) {
-  particles.push(new Particle(random(width/2-10, width/2+10),10,10));
- }
+function draw() {
+  background(0);  
 
+
+  Engine.run(engine);
+
+  if (frameCount % 60 === 0) {
+    particles.push(new Particle(random(width/2-10, width/2+10),10,10));
+   }
+   
  for (var j = 0; j < particles.length; j++) {
    particles[j].display();
  }
-
- for (var k = 0; k < divisions.length; k++) {
-  divisions[k].display();
-}
 
 for (var t = 0; t < plinkos.length; t++) {
   plinkos[t].display();
 }
 
-ground.display();
-  drawSprites();
+for (var k = 0; k < divisions.length; k++) {
+  divisions[k].display();
+}
 
+ground.display();
 }
